a66f1b3f21d08a21fce0870de643e43d3e4f41ad
Wei-Xinn <E0544325@u.nus.edu>
2023-09-13 23:00:39 +0800

    feat: add AddQuestionModal

--- assignment-1/src/page/QuestionPage.tsx
+++ assignment-1/src/page/QuestionPage.tsx
@@ -1,27 +1,64 @@
 import './QuestionPage.css';
+import { useEffect, useState } from 'react';
 import QuestionTable from '../component/Question/QuestionTable';
-import { questionSet } from '../data';
+import { questionSet } from '../GlobalData';
 import { Button } from '@mui/material';
-import { Modal } from '@mui/material';
+import AddQuestionModal from '../component/Question/AddQuestionModal/AddQuestionModal';
+import { questionString } from '../model/Question';
 
 const QuestionPage = () => {
 
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [questions, setQuestions] = useState<questionString[]>([]);
+  const [newTitle, setNewTitle] = useState('');
+  const [newCategories, setNewCategories] = useState('');
+  const [newComplexity, setNewComplexity] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const [newLink, setNewLink] = useState('');
+
+  function openModal() {
+    setModalIsVisible(true);
+  }
+
+  function closeModal() {
+    setModalIsVisible(false);
+  }
+
+  function submitHandler() {
+    const newQnStr = {
+      id: '1',
+      title: newTitle,
+      complexity: newComplexity,
+      categories: newCategories,
+      link: newLink
+    };
+    //TODO Validation
+    setQuestions(arr => [...arr, newQnStr]);
+    setModalIsVisible(false);
+  }
+
+  useEffect(() => {
+    setQuestions(questionSet);
+  }, [])
+
   return (
     <div id='question-page-container'>
+      <AddQuestionModal
+        isVisible={modalIsVisible}
+        closeHandler={closeModal}
+        titleSetter={setNewTitle}
+        linkSetter={setNewLink}
+        categoriesSetter={setNewCategories}
+        complexitySetter={setNewComplexity}
+        descriptionSetter={setNewDescription}
+        submitHandler={submitHandler}
+      />
       <div style={{ width: '50%' }}>
         <div id='button-container'>
-          <Modal
-            open={true}
-            onClose={() => { }}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <h1>asdfsadf</h1>
-          </Modal>
-          <Button id='add-btn' variant='contained'>Add</Button>
+          <Button id='add-btn' variant='contained' onClick={openModal}>Add</Button>
           <Button id='delete-btn' variant='contained' color='error'>Delete</Button>
         </div>
-        <QuestionTable data={questionSet} />
+        <QuestionTable data={questions} />
       </div>
     </div>
 
