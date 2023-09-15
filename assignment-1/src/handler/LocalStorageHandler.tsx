4cb503684b700b3e0261071ccad99e7feaafa3d8
Wei-Xinn <E0544325@u.nus.edu>
2023-09-15 13:19:42 +0800

    feat: add dummy method for getting question ID

--- assignment-1/src/handler/LocalStorageHandler.tsx
+++ assignment-1/src/handler/LocalStorageHandler.tsx
@@ -1,9 +1,8 @@
-import { Question, questionString } from "../model/Question";
+import { QuestionString } from "../model/Question";
 
 class LocalStorageHandler {
 
-  static saveQuestion(qnArr: questionString[]) {
-    // qnArr.map(qn => (localStorage.setItem("Questions", qnArr)))
+  static saveQuestion(qnArr: QuestionString[]) {
     window.localStorage.setItem("Questions", JSON.stringify(qnArr));
   }
 
@@ -11,6 +10,16 @@ class LocalStorageHandler {
     return JSON.parse(window.localStorage.getItem("Questions") || '{}');
   }
 
+  //Temp method for getting QuestionID
+  static getNextQuestionId() {
+    if (localStorage.getItem("nextId") === null) {
+      localStorage.setItem("nextId", '100')
+      return '100';
+    }
+    const nextId = (localStorage.getItem("nextId")!); //USING NON NULL OPERATOR
+    localStorage.setItem("nextId", (parseInt(nextId) + 1).toString());
+    return nextId;
+  }
 }
 
 export default LocalStorageHandler;
